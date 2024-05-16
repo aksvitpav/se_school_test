0ccb5fd84f467b37c381818b3e5d1b8d9299472e
Vitaly Aksonov <aksonov@quartsoft.com>
2024-05-17 02:33:20 +0300

    feat: add ide-helper package

--- app/Models/CurrencyRate.php
+++ app/Models/CurrencyRate.php
@@ -7,6 +7,28 @@ use App\Enums\CurrencyCodeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property CurrencyCodeEnum $currency_code
+ * @property mixed $buy_rate
+ * @property mixed $sale_rate
+ * @property string $fetched_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate query()
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereBuyRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereCurrencyCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereFetchedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereSaleRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CurrencyRate whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class CurrencyRate extends Model
 {
     use HasFactory;
