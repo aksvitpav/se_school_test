d1699c91f8c622409dd4591eab52531b7bbb63d9
Vitaly Aksonov <aksonov@quartsoft.com>
2024-05-19 00:26:08 +0300

    feat: add simple future tests

---
+++ tests/Feature/RateTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\GetCurrentRateAction;
+use App\Enums\CurrencyCodeEnum;
+use App\Models\CurrencyRate;
+use Mockery;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class RateTest extends TestCase
+{
+    //use RefreshDatabase;
+
+    public function test_it_returns_current_rate_successfully(): void
+    {
+        $currentRate = new CurrencyRate(
+            [
+                'currency_code' => CurrencyCodeEnum::USD->value,
+                'buy_rate' => 38.5,
+                'sale_rate' => 39.5,
+                'fetched_at' => now(),
+            ]
+        );
+
+        $getCurrentRateActionMock = Mockery::mock(GetCurrentRateAction::class);
+        $getCurrentRateActionMock->shouldReceive('execute')->andReturn($currentRate);
+
+        $this->app->instance(GetCurrentRateAction::class, $getCurrentRateActionMock);
+
+        $response = $this->getJson('/api/rate');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'buy' => 38.50,
+                'sale' => 39.50
+            ]);
+    }
+
+    public function test_it_returns_bad_request_when_rate_is_invalid(): void
+    {
+        $getCurrentRateActionMock = Mockery::mock(GetCurrentRateAction::class);
+        $getCurrentRateActionMock->shouldReceive('execute')->andReturn(null);
+
+        $this->app->instance(GetCurrentRateAction::class, $getCurrentRateActionMock);
+
+        $response = $this->getJson('/api/rate');
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST)
+            ->assertJson([
+                'message' => 'Invalid status value'
+            ]);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+}
---
+++ tests/Feature/SubscriptionTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\ExistSubscriberAction;
+use App\Actions\StoreSubscriberAction;
+use App\DTOs\SubscriberDTO;
+use App\Models\Subscriber;
+use Mockery;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class SubscriptionTest extends TestCase
+{
+    public function test_it_subscribes_email_successfully(): void
+    {
+        $email = 'test@example.com';
+        $requestData = ['email' => $email];
+
+        $dtoMatcher = Mockery::on(function ($dto) use ($requestData) {
+            return $dto instanceof SubscriberDTO && $dto->getEmail() === $requestData['email'];
+        });
+
+        $existSubscriberActionMock = Mockery::mock(ExistSubscriberAction::class);
+        $existSubscriberActionMock->shouldReceive('execute')
+            ->with($dtoMatcher)
+            ->andReturn(false);
+
+        $storeSubscriberActionMock = Mockery::mock(StoreSubscriberAction::class);
+        $storeSubscriberActionMock->shouldReceive('execute')
+            ->with($dtoMatcher)
+            ->andReturn(new Subscriber(['email' => $email]));
+
+        $this->app->instance(ExistSubscriberAction::class, $existSubscriberActionMock);
+        $this->app->instance(StoreSubscriberAction::class, $storeSubscriberActionMock);
+
+        $response = $this->postJson('/api/subscribe', $requestData);
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'message' => 'Email successfully added'
+            ]);
+    }
+
+    public function test_it_returns_conflict_when_email_already_exists(): void
+    {
+        $email = 'test@example.com';
+        $requestData = ['email' => $email];
+
+        $dtoMatcher = Mockery::on(function ($dto) use ($requestData) {
+            return $dto instanceof SubscriberDTO && $dto->getEmail() === $requestData['email'];
+        });
+
+        $existSubscriberActionMock = Mockery::mock(ExistSubscriberAction::class);
+        $existSubscriberActionMock->shouldReceive('execute')
+            ->with($dtoMatcher)
+            ->andReturn(true);
+
+        $storeSubscriberActionMock = Mockery::mock(StoreSubscriberAction::class);
+        $storeSubscriberActionMock->shouldNotReceive('execute');
+
+        $this->app->instance(ExistSubscriberAction::class, $existSubscriberActionMock);
+        $this->app->instance(StoreSubscriberAction::class, $storeSubscriberActionMock);
+
+        $response = $this->postJson('/api/subscribe', $requestData);
+
+        $response->assertStatus(Response::HTTP_CONFLICT)
+            ->assertJson([
+                'message' => 'Email already exists'
+            ]);
+    }
+
+    public function test_it_returns_validation_error_for_invalid_email(): void
+    {
+        $invalidEmail = 'invalid-email';
+        $requestData = ['email' => $invalidEmail];
+
+        $response = $this->postJson('/api/subscribe', $requestData);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+}
